73fb398c573ce019eca2aba64b79c0ce5357a324
SeaOfLee <l.richardson1@gmail.com>
2015-04-08 19:43:47 -0700

    Add jQuery for project images

--- js/app.js
+++ js/app.js
@@ -1,9 +1,65 @@
-$('.project_img').on('mouseenter',
-  function() {
-    $(this).addClass('animated bounce');
-  });
+// adds animation to project images when moused over
+$(function() {
+$('.project_img')
+  .on('mouseenter',
+    function() {
+      $(this).addClass('animated bounce');
+    })
 
-$('.project_img').on('mouseleave',
-  function() {
-    $(this).removeClass('animated bounce');
-  });
+  .on('mouseleave',
+    function() {
+      $(this).removeClass('animated bounce');
+    });
+});
+
+$(function() {
+$('#offerup')
+  .on('mouseenter',
+    function() {
+      $(this).attr('src', 'img/project_offerup.jpg');
+    })
+
+  .on('mouseleave',
+    function() {
+      $(this).attr('src', 'img/project_offerup_gray.jpg');
+    });
+});
+
+$(function() {
+$('#alumly')
+  .on('mouseenter',
+    function() {
+      $(this).attr('src', 'img/project_alumly.jpg');
+    })
+
+  .on('mouseleave',
+    function() {
+      $(this).attr('src', 'img/project_alumly_gray.jpg');
+    });
+});
+
+$(function() {
+$('#blog')
+  .on('mouseenter',
+    function() {
+      $(this).attr('src', 'img/project_blog.jpg');
+    })
+
+  .on('mouseleave',
+    function() {
+      $(this).attr('src', 'img/project_blog_gray.jpg');
+    });
+});
+
+$(function() {
+$('#ttt')
+  .on('mouseenter',
+    function() {
+      $(this).attr('src', 'img/project_ttt.jpg');
+    })
+
+  .on('mouseleave',
+    function() {
+      $(this).attr('src', 'img/project_ttt_gray.jpg');
+    });
+});
